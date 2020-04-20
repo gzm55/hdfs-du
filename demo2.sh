74e9c513c6e4c21714ba9dba583da6a54566cb2f
James Z.M. Gao <gaozhiming@360.cn>
2020-04-20 22:55:09 +0800

    make demo2 runnable

--- demo2.sh
+++ demo2.sh
@@ -8,10 +8,12 @@ cd target
 unzip hdfsdu-service-${HDFSDU_VERSION}-bin.zip
 cd hdfsdu-service-${HDFSDU_VERSION}
 pwd
+mkdir -p logs
 java -Xms2048m -Xmx2048m -cp 'lib/*' \
   com.twitter.hdfsdu.HdfsDu \
-    -http_port=$2 \
-    -title=$1 \
+    -http_port="$2" \
+    -title="$1" \
     -use_glog \
     -use_glog_formatter \
+    -log_dir logs \
     -input_path=../../src/main/resources/com/twitter/hdfsdu/data/example.txt
